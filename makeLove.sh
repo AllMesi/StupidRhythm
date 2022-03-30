a3f25a37a604b58f36942d3431556393644bee33
AllMesi <sketchgamermusic100az@gmail.com>
2022-03-30 15:26:16 +1000

    why am i doing so many small changes

--- makeLove.sh
+++ makeLove.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir out > logs.txt 2>> logs.txt
-sed -i '5s/.*/RELEASE\ =\ true/' globals.lua >> logs.txt
-rm -rf out/release >> logs.txt 2>> logs.txt
-mkdir out/release >> logs.txt
-mkdir out/release/bin >> logs.txt
-mkdir out/release/love >> logs.txt
-zip -9 -r StupidRhythm.love . -x /.git/* >> logs.txt
-mv StupidRhythm.love out/release/love >> logs.txt
-./love out/release/love/StupidRhythm.love >> logs.txt
+mkdir out
+sed -i '5s/.*/RELEASE\ =\ true/' globals.lua
+rm -rf out/release
+mkdir out/release
+mkdir out/release/bin
+mkdir out/release/love
+zip -9 -r StupidRhythm.love . -x /.git/*
+mv StupidRhythm.love out/release/love
+./love out/release/love/StupidRhythm.love
--- makeLoveDebug.sh
+++ makeLoveDebug.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir out > logs.txt 2>> logs.txt
-sed -i '5s/.*/RELEASE\ =\ false/' globals.lua >> logs.txt
-rm -rf out/debug >> logs.txt 2>> logs.txt
-mkdir out/debug >> logs.txt
-mkdir out/debug/bin >> logs.txt
-mkdir out/debug/love >> logs.txt
-zip -9 -r StupidRhythm.love . -x /.git/* >> logs.txt
-mv StupidRhythm.love out/debug/love >> logs.txt
-./love out/debug/love/StupidRhythm.love >> logs.txt
+mkdir out
+sed -i '5s/.*/RELEASE\ =\ false/' globals.lua
+rm -rf out/debug
+mkdir out/debug
+mkdir out/debug/bin
+mkdir out/debug/love
+zip -9 -r StupidRhythm.love . -x /.git/*
+mv StupidRhythm.love out/debug/love 
+./love out/debug/love/StupidRhythm.love
